c6df8be9b10734391e3010a5d493d3eeda3266e6
DevanshKyada27 <143169520+DevanshKyada27@users.noreply.github.com>
2024-02-09 17:46:18 +0530

    GFG_Nodejs_Workshop

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const Todo = require('../models/todo');
+const todo = require('../models/todo');
+const routes = express.Router();
+
+routes.get("/", async(req, res)=>{
+    const allTodo = await Todo.find();
+    res.render("home", {todo:allTodo});
+});
+
+routes.post("/add/todo", (req, res)=>{
+    const todo = req.body.todo;
+    const newTodo = new Todo({todo:todo});
+    newTodo.save().then(()=>{
+        console.log("Item has added");
+        res.redirect("/");
+    });
+});
+
+routes.get("/delete/todo/:_id", (req, res)=>{
+    const{_id} = req.params;
+    Todo.deleteOne({_id})
+    .then(()=>{
+        console.log("Deleted");
+        res.redirect("/");
+    });
+});
+
+module.exports = routes;
